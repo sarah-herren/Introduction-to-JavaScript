bf4f3eae30115aeffcfabdedabb2ea44eae338e9
Sarah Herren <sarah@Sarahs-Air.attlocal.net>
2020-11-15 13:51:51 -0600

    added task 7

--- index.js
+++ index.js
@@ -316,10 +316,23 @@ Using the grade function below do the following:
    below 60 = F
 */
   
-function grade(/*add your code here*/){
-    /*add your code here*/
+function grade(score){
+    if (score >= 90) {
+      console.log("A");
+    }
+      else if (score >= 80) {
+        console.log("B");
+      }
+      else if (score >= 70) {
+        console.log("C");
+      }
+      else if (score >= 60) {
+        console.log("D");
+      }
+      else { console.log("F");
+      }
   }
-  
+grade(71)
   
   
   
